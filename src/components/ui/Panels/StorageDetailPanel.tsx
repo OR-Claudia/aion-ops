a12e9cabce16a3189c2d6dcc622b017b18dd58e1
Claudia Nicolae <claudia.nicolae@oracle.com>
2025-09-05 13:19:33 +0300

    chore: fix LFS import

--- src/components/ui/Panels/StorageDetailPanel.tsx
+++ src/components/ui/Panels/StorageDetailPanel.tsx
@@ -5,7 +5,7 @@ import externalLinkIcon from "../../../assets/external-link.svg";
 import Button from "../Button";
 import VideoPlayer from "../VideoPlayer.tsx";
 import Tabs, { type TabItem } from "../Tabs";
-import aerialVideo from "../../../assets/videos/aerial.mp4";
+// import aerialVideo from "../../../assets/videos/aerial.mp4";
 // import RgbVideo from "../../../assets/videos/rgb_kuna.mp4";
 // import ThermoVideo from "../../../assets/videos/thermo_kuna.mp4";
 
@@ -76,7 +76,7 @@ const StorageDetailPanel: React.FC<StorageDetailPanelProps> = ({
 			id: "rgb",
 			label: "RGB",
 			// value: "http://193.123.68.104:8888/rgb_hls_stream_1/index.m3u8",
-			value: aerialVideo,
+			value: undefined,
 		},
 		{
 			id: "thermo",
